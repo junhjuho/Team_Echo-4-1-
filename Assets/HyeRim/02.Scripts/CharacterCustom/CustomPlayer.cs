a6877bb7a09fd51be4d86f22185e755992cabdda
heyhyenarm <narmhye@gmail.com>
2024-06-15 15:00:27 +0900

    CharacterCustom
     Jake만 되도록 설정함

--- Assets/HyeRim/02.Scripts/CharacterCustom/CustomPlayer.cs
+++ Assets/HyeRim/02.Scripts/CharacterCustom/CustomPlayer.cs
@@ -28,11 +28,11 @@ namespace NHR
         private void Init()
         {
             this.characters = this.GetComponentsInChildren<Character>();
-            foreach (var character in this.characters)
-            {
-                character.gameObject.SetActive(false);
-            }
-            this.characters[this.nowCharacterNum].gameObject.SetActive(true);
+            //foreach (var character in this.characters)
+            //{
+            //    character.gameObject.SetActive(false);
+            //}
+            //this.characters[this.nowCharacterNum].gameObject.SetActive(true);
             this.characterName.text = this.characters[this.nowCharacterNum].name;
         }
         public void ObserverUpdate(int characterNum, string textureName)
@@ -40,14 +40,14 @@ namespace NHR
             Debug.LogFormat("in CustomPlayer num : {0}, texture : {1}", characterNum, textureName);
             //전달된 정보 업데이트
             //이전 선택과 다를 경우
-            if (this.nowCharacterNum != characterNum)
-            {
-                //캐릭터 변신
-                this.characters[this.nowCharacterNum].gameObject.SetActive(false);
-                this.nowCharacterNum = characterNum;
-                this.characters[this.nowCharacterNum].gameObject.SetActive(true);
-                this.characterName.text = this.characters[this.nowCharacterNum].name;
-            }
+            //if (this.nowCharacterNum != characterNum)
+            //{
+            //    //캐릭터 변신
+            //    this.characters[this.nowCharacterNum].gameObject.SetActive(false);
+            //    this.nowCharacterNum = characterNum;
+            //    this.characters[this.nowCharacterNum].gameObject.SetActive(true);
+            //    this.characterName.text = this.characters[this.nowCharacterNum].name;
+            //}
             //텍스쳐 바꿈
             var mat = this.characters[this.nowCharacterNum].material;
             Debug.LogFormat("<color=yellow>material : {0}</color>", mat.name);
