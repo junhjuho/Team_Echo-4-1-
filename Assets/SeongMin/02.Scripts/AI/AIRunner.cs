77df97f0266ac4e2b4f44ed26fb63f1d9d0a1460
EETTT <152964052+FunnyP1ay@users.noreply.github.com>
2024-06-16 22:21:04 +0900

    AI 움직임

--- Assets/SeongMin/02.Scripts/AI/AIRunner.cs
+++ Assets/SeongMin/02.Scripts/AI/AIRunner.cs
@@ -8,10 +8,12 @@ namespace SeongMin
     public class AIRunner : MonoBehaviour
     {
         private WaitForSecondsRealtime oneSecond = new WaitForSecondsRealtime(1f);
+        private WaitUntil nextThink;
         private NavMeshAgent agent;
         private Animator animator;
         private Transform targetPosition;
-
+        private bool changeState = false;
+        private int rand = 0;
         public enum State
         {
             Die,
@@ -21,6 +23,7 @@ namespace SeongMin
         State state = State.Idle;
         private void Awake()
         {
+            nextThink = new WaitUntil(() => changeState);
             agent = GetComponent<NavMeshAgent>();
             animator = GetComponent<Animator>();
         }
@@ -28,9 +31,34 @@ namespace SeongMin
         {
             while (state != State.Die)
             {
-                yield return oneSecond;
+                switch (state)
+                {
+                    case State.Idle:
+                        break;
+                    case State.Move:
+                        NextTargetSetting();
+                        StartCoroutine(MoveCheck());
+                        break;
+                }
+                yield return nextThink;
+                changeState = false;
             }
             yield break;
+        }
+        private void NextTargetSetting()
+        {
+            rand = Random.Range(0,GameManager.Instance.inGameMapManager.inGameItemPositionList.Count);
+            targetPosition = GameManager.Instance.inGameMapManager.inGameItemPositionList[rand];
+            agent.SetDestination(targetPosition.position);
+        }
+        private IEnumerator MoveCheck()
+        {
+            if(Vector3.Distance(this.transform.position, targetPosition.position) < 3f)
+            {
+                changeState = true;
+                yield break;
+            }
+            yield return oneSecond;
         }
     }
 
