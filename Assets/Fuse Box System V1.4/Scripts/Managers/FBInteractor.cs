6e6573049d02e32ddc0fa2e4840757c6e3492e8b
Sparrow <94902059+Jack-93@users.noreply.github.com>
2024-06-17 17:41:50 +0900

    퓨즈박스에셋 추가 / 프리펩 목록 정리 / 프리펩 Grabtables 레이어 추가

---
+++ Assets/Fuse Box System V1.4/Scripts/Managers/FBInteractor.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+namespace FuseboxSystem
+{
+    public class FBInteractor : MonoBehaviour
+    {
+        [Header("Raycast Distance")]
+        [SerializeField] private int interactDistance = 5;
+
+        [Header("ObjectTag")]
+        [SerializeField] private string InteractiveTag = "InteractiveObject";
+
+        private FuseItem raycastedObj;
+        private Camera _camera;
+
+        private void Awake()
+        {
+            if (!TryGetComponent<Camera>(out _camera))
+            {
+                Debug.LogError("Camera component not found on the GameObject.");
+            }
+        }
+
+        void Update()
+        {
+            if (Physics.Raycast(_camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f)), transform.forward, out RaycastHit hit, interactDistance))
+            {
+                var selectedItem = hit.collider.GetComponent<FuseItem>();
+                if (selectedItem != null && selectedItem.CompareTag(InteractiveTag))
+                {
+                    raycastedObj = selectedItem;
+                    CrosshairChange(true);
+                }
+                else
+                {
+                    ClearRaycast();
+                }
+            }
+            else
+            {
+                ClearRaycast();
+            }
+
+            if (raycastedObj != null)
+            {
+                if (Input.GetKeyDown(FBInputManager.instance.interactKey))
+                {
+                    raycastedObj.ObjectInteract();
+                }
+            }
+        }
+        private void ClearRaycast()
+        {
+            if (raycastedObj != null)
+            {
+                CrosshairChange(false);
+                raycastedObj = null;
+            }
+        }
+
+        void CrosshairChange(bool on)
+        {
+            FBUIManager.instance.CrosshairChange(on);
+        }
+    }
+}
---
+++ Assets/Fuse Box System V1.4/Scripts/Managers/FBUIManager.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace FuseboxSystem
+{
+    public class FBUIManager : MonoBehaviour
+    {
+        [Header("Fuse Inventory Canvas")]
+        [SerializeField] private CanvasGroup fuseInventory = null;
+
+        [Header("Fuse UI")]
+        [SerializeField] private TMP_Text fuseAmountText = null;
+
+        [Header("Crosshair")]
+        [SerializeField] private Image crosshair = null;
+
+        [Header("Should persist?")]
+        [SerializeField] private bool persistAcrossScenes = true;
+
+        private bool isOpen;
+
+        public static FBUIManager instance;
+
+        private void Awake()
+        {
+            if (instance != null)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                instance = this;
+                if (persistAcrossScenes)
+                {
+                    DontDestroyOnLoad(gameObject);
+                }
+            }
+            FieldNullCheck();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(FBInputManager.instance.inventoryKey))
+            {
+                OpenInventory();
+            }
+        }
+
+        public void OpenInventory()
+        {
+            isOpen = !isOpen;
+            fuseInventory.alpha = isOpen ? 1 : 0;
+        }
+
+
+        public void UpdateFuseUI(int fusesAmount)
+        {
+            fuseAmountText.text = fusesAmount.ToString("0");
+        }
+
+        public void CrosshairChange(bool on)
+        {
+            crosshair.color = on ? Color.red : Color.white;
+        }
+
+        void FieldNullCheck()
+        {
+            // Checking each field and logging an error if it is null
+            CheckField(fuseInventory, "FuseInventory");
+            CheckField(fuseAmountText, "FuseAmountText");
+            CheckField(crosshair, "Crosshair");
+        }
+
+        void CheckField(Object field, string fieldName)
+        {
+            if (field == null)
+            {
+                Debug.LogError($"FieldNullCheck: {fieldName} is not set in the inspector!");
+            }
+        }
+    }
+}
+
