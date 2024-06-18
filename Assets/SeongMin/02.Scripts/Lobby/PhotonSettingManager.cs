4705a5abf7ceac0eceddac0061b3b9e9ed6da4a7
heyhyenarm <narmhye@gmail.com>
2024-06-18 21:47:45 +0900

    Update PhotonSettingManager.cs

--- Assets/SeongMin/02.Scripts/Lobby/PhotonSettingManager.cs
+++ Assets/SeongMin/02.Scripts/Lobby/PhotonSettingManager.cs
@@ -41,13 +41,26 @@ namespace SeongMin
             GameManager.Instance.lobbySceneManager.playerController = _player.GetComponent<PlayerController>();
 
             //Debug.LogFormat("<color=green>{0}</color>", photonView);
-            if (GameManager.Instance.lobbySceneManager.playerMission.photonView.IsMine)
+            foreach (Player player in PhotonNetwork.PlayerList)
             {
-                Debug.LogFormat("<color=green>{0}</color>", UIManager.Instance);
-                Debug.LogFormat("<color=green>{0}</color>", UIManager.Instance.robbySceneMenu);
-                int id = InfoManager.Instance.PlayerInfo.nowCharacterId;
-                UIManager.Instance.robbySceneMenu.SetPlayer(id);
+                if (player.CustomProperties.ContainsKey("playerCustom"))
+                {
+                    int id = (int)player.CustomProperties["playerCustom"];
+                    // 이 플레이어의 외형을 적용합니다.
+                    if (player == PhotonNetwork.LocalPlayer)
+                    {
+                        UIManager.Instance.robbySceneMenu.SetPlayer(id);
+                    }
+                }
             }
+
+            //if (GameManager.Instance.lobbySceneManager.playerMission.photonView.IsMine)
+            //{
+            //    Debug.LogFormat("<color=green>{0}</color>", UIManager.Instance);
+            //    Debug.LogFormat("<color=green>{0}</color>", UIManager.Instance.robbySceneMenu);
+            //    int id = InfoManager.Instance.PlayerInfo.nowCharacterId;
+            //    UIManager.Instance.robbySceneMenu.SetPlayer(id);
+            //}
 
 
             //캐릭터 커스텀 설정
