49b38158e84a4586870baaf92637bcf05a503e61
Sparrow <94902059+Jack-93@users.noreply.github.com>
2024-06-19 19:30:36 +0900

    ㅇ
    ㅇ

--- Assets/JaeWook/02_Scripts/CustomSocketInteractor.cs
+++ Assets/JaeWook/02_Scripts/CustomSocketInteractor.cs
@@ -5,50 +5,20 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 namespace Jaewook
 {
-    public class CustomSocketInteractable : XRSocketInteractor
+    public class CustomSocketInteractor : XRSocketInteractor
     {
-        AudioSource audiosource;
-        
-        protected override void Start()
+        public AudioSource audioSource;
+        public Door door;
+
+        private void Start()
         {
             this.selectEntered.AddListener(OnSelectEntered);
-            // this.selectExited.AddListener(SelectExit);
         }
 
-        /// <summary>
-        /// 물건을 Socket 적용 시
-        /// </summary>
-        /// <param name="args"></param>
         private void OnSelectEntered(SelectEnterEventArgs args)
         {
-            if (args.interactorObject.transform.TryGetComponent<FinalKey>(out FinalKey keyComponent))
-            {
-                // mesh renderer 삭제
-                args.interactableObject.transform.GetComponent<MeshRenderer>().enabled = false;
-
-                // collider가 있으면 삭제
-                if (args.interactableObject.transform.GetComponent<Collider>() != null)
-                {
-                    args.interactableObject.transform.GetComponent<Collider>().enabled = false;
-                }
-
-                args.interactableObject.transform.gameObject.SetActive(false);
-                
-
-
-            }
-
-
-
+            if
         }
-
-        /*
-        private void SelectExit(SelectExitEventArgs args)
-        {
-            args.interactableObject.transform.GetComponent<MeshRenderer>().enabled = true;
-
-        }
-        */
     }
 
 }
