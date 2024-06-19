766ef1a0a7678b134c73d42b58f1c42cbd664884
Sparrow <94902059+Jack-93@users.noreply.github.com>
2024-06-19 17:48:17 +0900

    맵 수정 / 3라운드 키 프리펩 추가 (스크립트중) /

--- Assets/JaeWook/02_Scripts/CustomSocketInteractor.cs
+++ Assets/JaeWook/02_Scripts/CustomSocketInteractor.cs
@@ -1,25 +1,52 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEngine.XR.Interaction.Toolkit;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
-//public class CustomSocketInteractor : XRSocketInteractor
-//{
-//    protected override void Start()
-//    {
-//        this.selectEntered.AddListener(SelectEnter);
-//        this.selectExited.AddListener(SelectExit);
+namespace Jaewook
+{
+    public class CustomSocketInteractable : XRSocketInteractor
+    {
+        AudioSource audiosource;
         
-//    }
+        protected override void Start()
+        {
+            this.selectEntered.AddListener(OnSelectEntered);
+            // this.selectExited.AddListener(SelectExit);
+        }
 
-//    private void SelectEnter(SelectEnterEventArgs args)
-//    {
-//        // mesh renderer -> 3D 화면 상에 그림
-//        args.interactableObject.transform.GetComponent<MeshRenderer>().enabled = false;   
-//    }
-//    private void SelectExit(SelectExitEventArgs args)
-//    {
-//        args.interactableObject.transform.GetComponent<MeshRenderer>().enabled = true;   
-        
-//    }
-//}
+        /// <summary>
+        /// 물건을 Socket 적용 시
+        /// </summary>
+        /// <param name="args"></param>
+        private void OnSelectEntered(SelectEnterEventArgs args)
+        {
+            if (args.interactorObject.transform.TryGetComponent<FinalKey>(out FinalKey keyComponent))
+            {
+                // mesh renderer 삭제
+                args.interactableObject.transform.GetComponent<MeshRenderer>().enabled = false;
+
+                // collider가 있으면 삭제
+                if (args.interactableObject.transform.GetComponent<Collider>() != null)
+                {
+                    args.interactableObject.transform.GetComponent<Collider>().enabled = false;
+                }
+
+                args.interactableObject.transform.gameObject.SetActive(false);
+
+            }
+
+
+
+        }
+
+        /*
+        private void SelectExit(SelectExitEventArgs args)
+        {
+            args.interactableObject.transform.GetComponent<MeshRenderer>().enabled = true;
+
+        }
+        */
+    }
+
+}
---
+++ Assets/JaeWook/02_Scripts/In Game Item/FinalKey.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Jaewook
+{
+    /// <summary>
+    /// 3라운드, Socket 형식으로 Door에 갖다 대면 Door가 열리는 애니메이션까지
+    /// </summary>
+    public class FinalKey : MonoBehaviour
+    {
+
+    }
+
+}
