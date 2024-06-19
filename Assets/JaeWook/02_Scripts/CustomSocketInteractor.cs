003b40694656378ae540a301d6633930e9af86c5
Sparrow <94902059+Jack-93@users.noreply.github.com>
2024-06-19 20:38:00 +0900

    ㅇㅇ

--- Assets/JaeWook/02_Scripts/CustomSocketInteractor.cs
+++ Assets/JaeWook/02_Scripts/CustomSocketInteractor.cs
@@ -7,17 +7,29 @@ namespace Jaewook
 {
     public class CustomSocketInteractor : XRSocketInteractor
     {
+        [Header("발동 효과 사운드")]
         public AudioSource audioSource;
+        [Header("나가는 문")]
         public Door door;
 
-        private void Start()
+        private  void Start()
         {
             this.selectEntered.AddListener(OnSelectEntered);
         }
 
         private void OnSelectEntered(SelectEnterEventArgs args)
         {
-            if
+            // 키가 
+            if(args.interactorObject.transform.TryGetComponent<FinalKey>(out FinalKey finalKeyComponenet))
+            {
+                args.interactableObject.transform.gameObject.SetActive(false);
+
+                door.OpenDoor();
+            }
+            else
+            {
+                Debug.LogError("이거 키 아닌디?");
+            }
         }
     }
 
