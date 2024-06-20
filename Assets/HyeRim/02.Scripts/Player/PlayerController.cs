6ae9dd6cc1b0b2464a099bb55190ade264519c89
heyhyenarm <narmhye@gmail.com>
2024-06-21 01:14:57 +0900

    PlayerCustom 동기화 완료!?

--- Assets/HyeRim/02.Scripts/Player/PlayerController.cs
+++ Assets/HyeRim/02.Scripts/Player/PlayerController.cs
@@ -39,10 +39,6 @@ namespace NHR
             }
             this.nowCharacterID = InfoManager.Instance.PlayerInfo.nowCharacterId;
 
-            //foreach (var character in this.characters)
-            //{
-            //    character.gameObject.SetActive(false);
-            //}
             this.characters[nowCharacterID].gameObject.SetActive(true);
             this.nowCharacter = this.characters[nowCharacterID];
             SeongMin.GameDB.Instance.playerMission.currentRunnerCharacrer = this.characters[nowCharacterID];
@@ -59,14 +55,7 @@ namespace NHR
         {
             Debug.Log("UpdateCharacterRPC");
 
-            //if (!photonView.IsMine) return;
             this.nowCharacterID = id;
-
-            //foreach (var character in this.characters)
-            //{
-            //    character.gameObject.SetActive(false);
-            //}
-
             this.ApplyCharacter(id);
         }
 
@@ -86,52 +75,6 @@ namespace NHR
             SeongMin.GameDB.Instance.playerMission.currentRunnerCharacrer = this.characters[id];
         }
 
-        //private void Update()
-        //{
-        //    if (!photonView.IsMine) return;
-        //    this.nowCharacterID = InfoManager.Instance.PlayerInfo.nowCharacterId;
-
-        //    //foreach (var character in this.characters)
-        //    //{
-        //    //    character.gameObject.SetActive(false);
-        //    //}
-        //    this.characters[nowCharacterID].gameObject.SetActive(true);
-        //    Debug.Log(this.nowCharacterID);
-        //}
-        //private IEnumerator Start()
-        //{
-        //    // 나의 클라이언트가 네트워크에 연결될때까지 기달리기
-        //    yield return new WaitUntil(() => PhotonNetwork.IsConnected);
-
-        //    //캐릭터 커스텀 설정
-        //    var id = 0;
-        //    this.photonView.RPC("ApplyCustom", RpcTarget.AllBufferedViaServer, id);
-        //}
-
-        //[PunRPC]
-        //public void ApplyCustom(int characterID)
-        //{
-        //    //내 것이 아니면 return
-        //    if (!photonView.IsMine) return;
-        //    if (this.photonView.IsMine)
-        //    {
-        //        //int id = InfoManager.Instance.PlayerInfo.nowCharacterId;
-        //        //string colorName = InfoManager.Instance.PlayerInfo.nowClothesColorName;
-        //        Debug.LogFormat("<color=yellow>Set id {0}</color>", characterID);
-        //        foreach (var character in this.characters)
-        //        {
-        //            character.gameObject.SetActive(false);
-        //        }
-
-        //        this.characters[characterID].gameObject.SetActive(true);
-        //        SeongMin.GameDB.Instance.playerMission.currentRunnerCharacrer = this.characters[characterID];
-        //        //var mat = this.characters[characterID].material;
-        //        //Debug.LogFormat("<color=yellow>character : {0}, texture : {1}</color>", characterID, colorName);
-        //        //mat.mainTexture = Resources.Load<Texture>("ClothesTexture/" + id + colorName);
-
-        //    }
-
-        //}
 
         public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
         {
@@ -142,106 +85,5 @@ namespace NHR
             //else this.nowCharacterID = (int)stream.ReceiveNext();
         }
     }
-    //public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
-    //{
-
-    //    [Header("캐릭터 커스텀")]
-    //    public Character[] characters;
-    //    public Character nowCharacter;
-
-    //    public SmartWatchCustomInteractable watch;
-
-    //    public int nowCharacterID;
-    //    private void Awake()
-    //    {
-    //        GameDB.Instance.playerController = this;
-
-    //        if (this.watch == null) this.watch = GetComponentInChildren<SmartWatchCustomInteractable>();
-
-    //        this.nowCharacterID = InfoManager.Instance.PlayerInfo.nowCharacterId;
-    //    }
-    //    private void Start()
-    //    {
-    //        this.nowCharacterID = InfoManager.Instance.PlayerInfo.nowCharacterId;
-
-    //        //foreach (var character in this.characters)
-    //        //{
-    //        //    character.gameObject.SetActive(false);
-    //        //}
-    //        this.characters[nowCharacterID].gameObject.SetActive(true);
-    //        this.nowCharacter = this.characters[nowCharacterID];
-    //        SeongMin.GameDB.Instance.playerMission.currentRunnerCharacrer = this.characters[nowCharacterID];
-    //    }
-    //    public void UpdateCharacter(int id)
-    //    {
-    //        if (!photonView.IsMine) return;
-    //        this.nowCharacterID = id;
-
-    //        //foreach (var character in this.characters)
-    //        //{
-    //        //    character.gameObject.SetActive(false);
-    //        //}
-    //        this.nowCharacter.gameObject.SetActive(false);
-    //        this.characters[nowCharacterID].gameObject.SetActive(true);
-    //        this.nowCharacter = this.characters[nowCharacterID];
-    //        SeongMin.GameDB.Instance.playerMission.currentRunnerCharacrer = this.characters[nowCharacterID];
-    //    }
-
-    //    //private void Update()
-    //    //{
-    //    //    if (!photonView.IsMine) return;
-    //    //    this.nowCharacterID = InfoManager.Instance.PlayerInfo.nowCharacterId;
-
-    //    //    //foreach (var character in this.characters)
-    //    //    //{
-    //    //    //    character.gameObject.SetActive(false);
-    //    //    //}
-    //    //    this.characters[nowCharacterID].gameObject.SetActive(true);
-    //    //    Debug.Log(this.nowCharacterID);
-    //    //}
-    //    //private IEnumerator Start()
-    //    //{
-    //    //    // 나의 클라이언트가 네트워크에 연결될때까지 기달리기
-    //    //    yield return new WaitUntil(() => PhotonNetwork.IsConnected);
-
-    //    //    //캐릭터 커스텀 설정
-    //    //    var id = 0;
-    //    //    this.photonView.RPC("ApplyCustom", RpcTarget.AllBufferedViaServer, id);
-    //    //}
-
-    //    //[PunRPC]
-    //    //public void ApplyCustom(int characterID)
-    //    //{
-    //    //    //내 것이 아니면 return
-    //    //    if (!photonView.IsMine) return;
-    //    //    if (this.photonView.IsMine)
-    //    //    {
-    //    //        //int id = InfoManager.Instance.PlayerInfo.nowCharacterId;
-    //    //        //string colorName = InfoManager.Instance.PlayerInfo.nowClothesColorName;
-    //    //        Debug.LogFormat("<color=yellow>Set id {0}</color>", characterID);
-    //    //        foreach (var character in this.characters)
-    //    //        {
-    //    //            character.gameObject.SetActive(false);
-    //    //        }
-
-    //    //        this.characters[characterID].gameObject.SetActive(true);
-    //    //        SeongMin.GameDB.Instance.playerMission.currentRunnerCharacrer = this.characters[characterID];
-    //    //        //var mat = this.characters[characterID].material;
-    //    //        //Debug.LogFormat("<color=yellow>character : {0}, texture : {1}</color>", characterID, colorName);
-    //    //        //mat.mainTexture = Resources.Load<Texture>("ClothesTexture/" + id + colorName);
-
-    //    //    }
-
-    //    //}
-
-    //    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    //    {
-    //        if (stream.IsWriting)   //isMine인 경우
-    //        {
-    //            stream.SendNext(this.nowCharacterID);
-    //        }
-    //        else this.nowCharacterID = (int)stream.ReceiveNext();
-    //    }
-    //}
 
 }
