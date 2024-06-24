cf59474550c3c712dccafb40a02b99dbe0709c5d
heyhyenarm <narmhye@gmail.com>
2024-06-24 13:52:36 +0900

    lobby 아이템 수정
    npc 플레이어 수정만 하면 끝

--- Assets/SeongMin/02.Scripts/Lobby/PhotonSettingManager.cs
+++ Assets/SeongMin/02.Scripts/Lobby/PhotonSettingManager.cs
@@ -39,6 +39,8 @@ namespace SeongMin
             var _player = PhotonNetwork.Instantiate("Player", Vector3.up, Quaternion.identity);
             _player.transform.position = GameManager.Instance.lobbySceneManager.playerSpawnPointList[0].position;
             GameManager.Instance.lobbySceneManager.playerMission = _player.GetComponent<PlayerMission>();
+            //튜토리얼 임의 지정
+            GameManager.Instance.lobbySceneManager.playerMission.isChaser = true;
             GameManager.Instance.lobbySceneManager.playerController = _player.GetComponent<PlayerController>();
             UIManager.Instance.robbySceneMenu.customPlayer.playerController = _player.GetComponent<PlayerController>();
 
