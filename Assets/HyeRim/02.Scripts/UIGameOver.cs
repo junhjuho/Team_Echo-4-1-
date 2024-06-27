0044f2cf1d6e965cbaec7adab1c7e09e37d5c0e2
jungjuho <133942564+junhjuho@users.noreply.github.com>
2024-06-27 11:12:55 +0900

    Update UIGameOver.cs

--- Assets/HyeRim/02.Scripts/UIGameOver.cs
+++ Assets/HyeRim/02.Scripts/UIGameOver.cs
@@ -8,19 +8,19 @@ public class UIGameOver : MonoBehaviour
 {
     public TMP_Text txtGameResult;
 
-    public void IsWin(bool isWin)
-    {
-        string role;
-        if (GameDB.Instance.playerMission.isChaser) role = "복수자";
-        else role = "생존자";
+    //public void IsWin(bool isWin)
+    //{
+    //    string role;
+    //    if (GameDB.Instance.playerMission.isChaser) role = "복수자";
+    //    else role = "생존자";
 
-        string dialog;
+    //    string dialog;
 
-        if (isWin) dialog = string.Format("{0} 승리", role);
-        else dialog = string.Format("{0} 패배", role);
+    //    if (isWin) dialog = string.Format("{0} 승리", role);
+    //    else dialog = string.Format("{0} 패배", role);
 
-        StartCoroutine(this.CTypingDialog(dialog));
-    }
+    //    StartCoroutine(this.CTypingDialog(dialog));
+    //}
 
     IEnumerator CTypingDialog(string dialog)
     {
